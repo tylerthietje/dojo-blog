1edabdd8ead3c6b79e20e510a53cb8713d4aeae3
Tyler Thietje <tylerthietje@gmail.com>
2022-07-04 17:12:52 -0500

    added loading... message that displays until JSON data is loaded

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,27 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data)
-    }) 
+    setTimeout(() => {
+
+    
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data)
+          setIsPending(false);
+        });
+    }, 2000);
     }, []); // dependency array limits when the useEffect hook will render the component
 
   return (
     <div className="home">
+      { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
       {/* Conditional templating: evaluates the left side of the apersands first and finds it true, then it moves to the right and evaluates that*/}
     </div>
