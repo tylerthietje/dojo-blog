968765d25efb15f481cfc2fff3ba6e27430c040b
Tyler Thietje <tylerthietje@gmail.com>
2022-07-04 17:56:52 -0500

    added custom hook for the fetch logic that is much more reusable

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,13 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-
-  useEffect(() => {
-    setTimeout(() => {    
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        if (!res.ok) { // error coming back from server
-          throw Error('could not fetch the data for that resource');
-        } 
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-        setError(null);
-      })
-        .catch(err => {
-          setIsPending(false);
-        // auto catches network / connection error
-        setIsPending(false);
-        setError(err.message);
-      })
-    }, 1000);
-  }, []) // dependency array limits when the useEffect hook will render the component
-
+  const { data: blogs, isPending, Error } = useFetch('http://localhost:8000/blogs');
+  
   return (
     <div className="home">
-      { error && <div>{error}</div> }
+      { Error && <div>{ Error }</div> }
       { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
       {/* Conditional templating: evaluates the left side of the apersands first and finds it true, then it moves to the right and evaluates that*/}
