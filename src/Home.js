2082e7a12dedaf461afa76ed5401bdafa91c7296
Tyler Thietje <tylerthietje@gmail.com>
2022-07-04 17:31:37 -0500

    added logic to handle local errors and network errors

--- src/Home.js
+++ src/Home.js
@@ -4,25 +4,35 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
 
   useEffect(() => {
-    setTimeout(() => {
-
-    
+    setTimeout(() => {    
       fetch('http://localhost:8000/blogs')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setBlogs(data)
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+        .catch(err => {
           setIsPending(false);
-        });
-    }, 2000);
-    }, []); // dependency array limits when the useEffect hook will render the component
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, []) // dependency array limits when the useEffect hook will render the component
 
   return (
     <div className="home">
+      { error && <div>{error}</div> }
       { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
       {/* Conditional templating: evaluates the left side of the apersands first and finds it true, then it moves to the right and evaluates that*/}
