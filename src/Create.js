9b1cfa4b190385cdf972ae42d171c31a1ff55d31
Tyler Thietje <tylerthietje@gmail.com>
2022-07-05 21:21:18 -0500

    added functionality to form fields to capture inputs and store them as an object

--- src/Create.js
+++ src/Create.js
@@ -2,43 +2,46 @@ import React from 'react';
 import { useState } from 'react';
 
 const Create = () => {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('Mario');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Mario');
 
-    return ( 
-        <div className="create">
-            <h2>Add a New Blog</h2>
-            <form>
-                <label>Blog title:</label>
-                <input
-                    type="text"
-                    required
-                    value={title}
-                    onChange={(e)=> setTitle(e.target.value)}
-                />
-                <label>Blog body:</label>
-                <textarea
-                    required
-                    value={body}
-                    onChange={(e) => setBody(e.target.Value)}
-                ></textarea>
-                <label>Blog author:</label>
-                <select
-                    value={author}
-                    onChange={(e) => setAuthor(e.target.value)}
-                >
-                    <option value="Mario">Mario</option>
-                    <option value="Yoshi">Yoshi</option>
-                </select>
-                <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    
+    console.log(blog);
+  }
 
-            </form>
-        </div>
-    );
-}
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={ handleSubmit }>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
+    </div>
+  );
+};
 
 export default Create;
